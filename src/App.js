db5e2b701396957ede277ca8397387dd488b428b
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-07 07:14:24 +0300

    Changed Button style, fixed code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
+import React from 'react';
 import './App.scss';
+import Button from './Button';
+// const values = ["0, 1, 2, 3, 4, 5, 6, 7, 8, 9, C, +, -, x, =, /, ."]
+// const buttonsRegex = /\d|-|\+|=|C|\.|\/|\*|x/gi;
 
 export default function App() {
   return (
     <>
     <div id="calculator-container">
       <div id="calculator">
-      <p id="display"></p>
-      <button id="clear">C</button>
-      <button id="add">+</button>
-      <button id="subtract">-</button>
-      <button id="multiply">x</button>
-      <button id="divide">÷</button>
-      <button id="equals">=</button>
-      <button id="zero">0</button>
-      <button id="one">1</button>
-      <button id="two">2</button>
-      <button id="three">3</button>
-      <button id="four">4</button>
-      <button id="five">5</button>
-      <button id="six">6</button>
-      <button id="seven">7</button>
-      <button id="eight">8</button>
-      <button id="nine">9</button>
-      <button id='decimal'>.</button>
+        <p id="display">hey</p>
+        <div id="button-container">
+          <Button id="clear" text="C"/>
+          <Button id="divide" text="/"/>
+          <Button id="multiply" text="*"/>
+          <Button id="subtract" text="-"/>
+          <Button id="add" text="+"/>
+          <Button id="equals" text="="/>
+          <Button id="zero" text="0"/>
+          <Button id="one" text="1"/>
+          <Button id="two" text="2"/>
+          <Button id="three" text="3"/>
+          <Button id="four" text="4"/>
+          <Button id="five" text="5"/>
+          <Button id="six" text="6"/>
+          <Button id="seven" text="7"/>
+          <Button id="eight" text="8"/>
+          <Button id="nine" text="9"/>
+          <Button id='decimal' text='.'/>
+          </div>
       </div>
     </div>
     </>
--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
 import handleClick from './handleClick';
+
 export default function Button (props) {
     return (
         <button id={props.id} onClick={handleClick}>{props.text}</button>
