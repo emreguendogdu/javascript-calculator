c29dde029545313e5e4d2ad7ade4387def46365c
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-08 12:27:14 +0300

    Added key regex, changed handleClick function to handleValue: now working with both click & keydowns, improved handleValue function to work well with clear & set the display

--- src/App.js
+++ src/App.js
@@ -1,64 +1,68 @@
+/* eslint-disable no-eval */
 import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Buttons from './Buttons';
 
 function App () {
+
   const [display, setDisplay] = useState("");
   const [result, setResult] = useState("");
-  const ops = ['/', '*', '-', '+', '.'];
+  const ops = ['/', '*', '-', '+', '.', "C"];
 
   useEffect(() => {
+    
     const handleKeyDown = (e) => {
-      handleClick({key: e.key});
+      if (!/([0-9+\-*/.=Cc]|Enter|Backspace)/.test(e.key) || e.key === "Control") {
+        return;
+      }
+      handleValue(e.key);
     };
     document.addEventListener('keydown', handleKeyDown);
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, []);
+  });
 
 
-  function handleClick(e) {
-    // If target exists: take clicked key, otherwise take pressed key
-    let clickedKey = e.target ? e.target.innerHTML : e.key;
+  function handleValue(e) {
+  let key = e.target ? e.target.innerHTML : e;
 
-    // Check if the key pressed is a valid input
-    if (!/[0-9+\-*/.=C]/.test(clickedKey)) {
-      return;
-    }
-    
-    if (
-      (ops.includes(clickedKey) && display === '')  || 
-      (ops.includes(clickedKey) && ops.includes(display.slice(-1))) || 
-      (display === '' && clickedKey === "0")
-      ) 
-      {return;}
-    if (clickedKey === "C") {
-      setDisplay("");
-      setResult("");
-      return;
-    }
-    if (clickedKey === "+/-") {
-      // If display is not 0, give opposite value, stringify is for display.slice
-      if (display !== "") {
-        setDisplay((display * -1).toString());
-        setResult((display * -1).toString());
-        return;
-      }
-    }
-    if (clickedKey === "=") {
-      setDisplay(result);
-      return;
-    }
-    setDisplay(display + clickedKey);
+  if (
+    (ops.includes(key) && display === '')  || 
+    (ops.includes(key) && ops.includes(display.slice(-1))) || 
+    (display === '' && key === "0") ||
+    (display === '' && key === "= ")
+    ) 
+    {return;}
 
-    if (!ops.includes(clickedKey)) {
-      let result = eval(display + clickedKey);
-      let formattedResult = Number(result.toFixed(2));
-      setResult(formattedResult.toString())
-    }
+  if ((display !== "" && key.toLowerCase() === "c") ||
+  (display !== "" && key === "Backspace")) {
+    setDisplay("");
+    setResult("");
+    return;
   }
 
+  if (key === "+/-") {
+    if (display !== "") {
+      setDisplay((display * -1).toString());
+      setResult((display * -1).toString());
+      return;
+    }
+  } 
+
+  if (key === "=" || key === "Enter") {
+    setDisplay(result);
+    return;
+  } else if (!ops.includes(key)) {
+    let calcResult = eval(display + key);
+    let formattedResult = Number(calcResult.toFixed(2));
+    setResult(formattedResult.toString())
+  }
+  
+  setDisplay(prevDisplay => prevDisplay + key);
+  }
+
+  
   return (
     <>
       <div id="calculator">
@@ -67,7 +71,7 @@ function App () {
           {display || "0"}
         </div>
         <div id="button-container">
-          <Buttons onClick={handleClick} />
+          <Buttons onClick={handleValue} />
         </div>
       </div>
     </>
