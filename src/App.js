8c2f0eed1831d6d2c0254f6f7c03edc3d1a89a9e
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2023-11-08 11:15:25 +0300

    Added calculator basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Buttons from './Buttons';
-// const values = ["0, 1, 2, 3, 4, 5, 6, 7, 8, 9, C, +, -, x, =, /, ."]
-// const buttonsRegex = /\d|-|\+|=|C|\.|\/|\*|x/gi;
 
 function App () {
-  const [display, setDisplay] = React.useState("0");
-  
+  const [display, setDisplay] = useState("");
+  const [result, setResult] = useState("");
+  const ops = ['/', '*', '-', '+', '.'];
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      handleClick({key: e.key});
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+
+  function handleClick(e) {
+    // If target exists: take clicked key, otherwise take pressed key
+    let clickedKey = e.target ? e.target.innerHTML : e.key;
+
+    // Check if the key pressed is a valid input
+    if (!/[0-9+\-*/.=C]/.test(clickedKey)) {
+      return;
+    }
+    
+    if (
+      (ops.includes(clickedKey) && display === '')  || 
+      (ops.includes(clickedKey) && ops.includes(display.slice(-1))) || 
+      (display === '' && clickedKey === "0")
+      ) 
+      {return;}
+    if (clickedKey === "C") {
+      setDisplay("");
+      setResult("");
+      return;
+    }
+    if (clickedKey === "+/-") {
+      // If display is not 0, give opposite value, stringify is for display.slice
+      if (display !== "") {
+        setDisplay((display * -1).toString());
+        setResult((display * -1).toString());
+        return;
+      }
+    }
+    if (clickedKey === "=") {
+      setDisplay(result);
+      return;
+    }
+    setDisplay(display + clickedKey);
+
+    if (!ops.includes(clickedKey)) {
+      let result = eval(display + clickedKey);
+      let formattedResult = Number(result.toFixed(2));
+      setResult(formattedResult.toString())
+    }
+  }
+
   return (
-  <>
-  <div id="calculator-container">
-    <div id="calculator">
-      <div id="display">{display}</div>
-      <div id="button-container">
-        <Buttons />
+    <>
+      <div id="calculator">
+        <div id="display">
+          {result ? <span>({result})</span> : ""}
+          {display || "0"}
         </div>
-    </div>
-  </div>
-  </>
+        <div id="button-container">
+          <Buttons onClick={handleClick} />
+        </div>
+      </div>
+    </>
   )
 };
 
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,25 +1,28 @@
-import HandleClick from "./HandleClick"
 import React from "react";
 
 export default class Buttons extends React.Component {
     state = {
         buttons: [
+            // {id: "divide", text: "÷" , className: "ops"}, // TODO:  Find a way to display ÷ but not get an UTF error.
+            {id: "divide", text: "/" , className: "ops"},
+            {id: "percentage", text: "%"},
+            {id: "plusminus", text: "+/-"},
             {id: "clear", text: "C"},
-            {id: "divide", text: "/"},
-            {id: "multiply", text: "*"},
-            {id: "subtract", text: "-"},
-            {id: "add", text: "+"},
-            {id: "equals", text: "="},
-            {id: "zero", text: "0"},
-            {id: "one", text: "1"},
-            {id: "two", text: "2"},
-            {id: "three", text: "3"},
-            {id: "four", text: "4"},
-            {id: "five", text: "5"},
-            {id: "six", text: "6"},
-            {id: "seven", text: "7"},
-            {id: "eight", text: "8"},
+            {id: "multiply", text: "*", className: "ops"},
             {id: "nine", text: "9"},
+            {id: "eight", text: "8"},
+            {id: "seven", text: "7"},
+            {id: "add", text: "+", className: "ops"},
+            {id: "six", text: "6"},
+            {id: "five", text: "5"},
+            {id: "four", text: "4"},
+            {id: "subtract", text: "-", className: "ops"},
+            {id: "three", text: "3"},
+            {id: "two", text: "2"},
+            {id: "one", text: "1"},
+            {id: "equals", text: "=", className: "ops"},
+            {id: "decimal", text: "."},
+            {id: "zero", text: "0"},
         ] 
     }
     render() {
@@ -28,7 +31,7 @@ export default class Buttons extends React.Component {
             <React.Fragment>
                 {buttons.map(button => {
                     return (
-                        <button key={button.id} id={button.id} onClick={this.props.handleClick}>{button.text}</button>
+                        <button key={button.id} id={button.id} onClick={this.props.onClick} className={button.className}>{button.text}</button>
                     )
                 })}
             </React.Fragment>
